522cf86ef089409e58311140441d94bebef262e9
Matteo Pace <pace.matteo96@gmail.com>
2026-07-28 12:16:32 +0200

    fix coraza/ftw/crs bumps

--- wafbench/wafbench_test.go
+++ wafbench/wafbench_test.go
@@ -1,9 +1,7 @@
 package wafbench
 
 import (
-	"archive/zip"
 	"bufio"
-	"bytes"
 	_ "embed"
 	"fmt"
 	"io"
@@ -16,152 +14,32 @@ import (
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/bmatcuk/doublestar/v4"
+	coreruleset "github.com/corazawaf/coraza-coreruleset/v4"
+	crstests "github.com/corazawaf/coraza-coreruleset/v4/tests"
 	"github.com/corazawaf/coraza/v3"
 	txhttp "github.com/corazawaf/coraza/v3/http"
 	"github.com/corazawaf/coraza/v3/types"
-	"github.com/coreruleset/go-ftw/config"
-	"github.com/coreruleset/go-ftw/output"
-	"github.com/coreruleset/go-ftw/runner"
-	"github.com/coreruleset/go-ftw/test"
+	albedo "github.com/coreruleset/albedo/server"
+	"github.com/coreruleset/go-ftw/v2/config"
+	"github.com/coreruleset/go-ftw/v2/output"
+	"github.com/coreruleset/go-ftw/v2/runner"
+	"github.com/coreruleset/go-ftw/v2/test"
 	"github.com/rs/zerolog"
 )
-
-//go:embed coreruleset-32e6d80419d386a330ddaf5e60047a4a1c38a160.zip
-var crsZip []byte
 
 //go:embed coraza.conf-recommended
 var confRecommended string
 
 func BenchmarkWAF(b *testing.B) {
-	crsReader, err := zip.NewReader(bytes.NewReader(crsZip), int64(len(crsZip)))
-	if err != nil {
-		b.Fatal(err)
-	}
-
-	crs, err := fs.Sub(crsReader, "coreruleset-32e6d80419d386a330ddaf5e60047a4a1c38a160")
-	if err != nil {
-		b.Fatal(err)
-	}
-
-	conf := coraza.NewWAFConfig()
-	customTestingConfig := `
-SecResponseBodyMimeType text/plain
-SecDefaultAction "phase:3,log,auditlog,pass"
-SecDefaultAction "phase:4,log,auditlog,pass"
-SecAction "id:900005,\
-  phase:1,\
-  nolog,\
-  pass,\
-  ctl:ruleEngine=DetectionOnly,\
-  ctl:ruleRemoveById=910000,\
-  # Interferes with ftw log scanning
-  ctl:ruleRemoveById=920250,\
-  setvar:tx.paranoia_level=4,\
-  setvar:tx.crs_validate_utf8_encoding=1,\
-  setvar:tx.arg_name_length=100,\
-  setvar:tx.arg_length=400,\
-  setvar:tx.total_arg_length=64000,\
-  setvar:tx.max_num_args=255,\
-  setvar:tx.max_file_size=64100,\
-  setvar:tx.combined_file_sizes=65535"
-# Write the value from the X-CRS-Test header as a marker to the log
-SecRule REQUEST_HEADERS:X-CRS-Test "@rx ^.*$" \
-  "id:999999,\
-  phase:1,\
-  log,\
-  msg:'X-CRS-Test %{MATCHED_VAR}',\
-  pass,\
-  t:none"
-`
-	// Configs are loaded with a precise order:
-	// 1. Coraza config
-	// 2. Custom Rules for testing and eventually overrides of the basic Coraza config
-	// 3. CRS basic config
-	// 4. CRS rules (on top of which are applied the previously defined SecDefaultAction)
-	conf = conf.
-		WithRootFS(crs).
-		WithDirectives(confRecommended).
-		WithDirectives(customTestingConfig).
-		WithDirectives("Include crs-setup.conf.example").
-		WithDirectives("Include rules/*.conf")
-
-	errorPath := filepath.Join(b.TempDir(), "error.log")
-	errorFile, err := os.Create(errorPath)
-	if err != nil {
-		b.Fatalf("failed to create error log: %v", err)
-	}
-	errorWriter := bufio.NewWriter(errorFile)
-	conf = conf.WithErrorLogger(func(rule types.MatchedRule) {
-		msg := rule.ErrorLog(0)
-		if _, err := io.WriteString(errorWriter, msg); err != nil {
-			b.Fatal(err)
-		}
-		if err := errorWriter.Flush(); err != nil {
-			b.Fatal(err)
-		}
-	})
-
-	waf, err := coraza.NewWAF(conf)
-	if err != nil {
-		b.Fatal(err)
-	}
-
-	s := httptest.NewServer(txhttp.WrapHandler(waf, b.Logf, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Emulated httpbin behaviour: /anything endpoint acts as an echo server, writing back the request body
-		if r.URL.Path == "/anything" {
-			defer r.Body.Close()
-			w.Header().Set("Content-Type", "text/plain")
-			_, err = io.Copy(w, r.Body)
-			if err != nil {
-				b.Fatalf("handler can not read request body: %v", err)
-			}
-		} else {
-			fmt.Fprintf(w, "Hello!")
-		}
-	})))
+	errorPath, s := setupWAF(b)
 	defer s.Close()
 
 	b.Run("FTW", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			var tests []test.FTWTest
-			err = doublestar.GlobWalk(crs, "tests/regression/tests/**/*.yaml", func(path string, d os.DirEntry) error {
-				yaml, err := fs.ReadFile(crs, path)
-				if err != nil {
-					return err
-				}
-				t, err := test.GetTestFromYaml(yaml)
-				if err != nil {
-					return err
-				}
-				tests = append(tests, t)
-				return nil
-			})
-			if err != nil {
-				b.Fatal(err)
-			}
-
-			u, _ := url.Parse(s.URL)
-			host := u.Hostname()
-			port, _ := strconv.Atoi(u.Port())
-			// TODO(anuraaga): Don't use global config for FTW for better support of programmatic.
-			zerolog.SetGlobalLevel(zerolog.InfoLevel)
-			_ = config.NewConfigFromFile(".ftw.yml")
-			config.FTWConfig.LogFile = errorPath
-			config.FTWConfig.TestOverride.Input.DestAddr = &host
-			config.FTWConfig.TestOverride.Input.Port = &port
-
-			res, err := runner.Run(tests, runner.Config{
-				ShowTime: false,
-			}, output.NewOutput("quiet", os.Stdout))
-			if err != nil {
-				b.Fatal(err)
-			}
-
-			if len(res.Stats.Failed) > 0 {
-				b.Errorf("failed tests: %v", res.Stats.Failed)
-			}
+			runFTW(b, errorPath, s)
 		}
 	})
 
@@ -176,4 +54,138 @@ SecRule REQUEST_HEADERS:X-CRS-Test "@rx ^.*$" \
 			}
 		})
 	}
+}
+
+// runFTW runs the CRS regression suite against the WAF fronting server.
+func runFTW(tb testing.TB, errorPath string, s *httptest.Server) {
+	tb.Helper()
+
+	var tests []*test.FTWTest
+	err := doublestar.GlobWalk(crstests.FS, "**/*.yaml", func(path string, d os.DirEntry) error {
+		yaml, err := fs.ReadFile(crstests.FS, path)
+		if err != nil {
+			return err
+		}
+		t, err := test.GetTestFromYaml(yaml, path)
+		if err != nil {
+			return err
+		}
+		tests = append(tests, t)
+		return nil
+	})
+	if err != nil {
+		tb.Fatal(err)
+	}
+	if len(tests) == 0 {
+		tb.Fatal("no tests found")
+	}
+
+	u, _ := url.Parse(s.URL)
+	host := u.Hostname()
+	port, _ := strconv.Atoi(u.Port())
+	// TODO(anuraaga): Don't use global config for FTW for better support of programmatic.
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	ftwConf, err := config.NewConfigFromFile(".ftw.yml")
+	if err != nil {
+		tb.Fatal(err)
+	}
+	ftwConf.LogFile = errorPath
+	ftwConf.TestOverride.Overrides.DestAddr = &host
+	ftwConf.TestOverride.Overrides.Port = &port
+
+	runnerConf := config.NewRunnerConfiguration(ftwConf)
+	runnerConf.ShowTime = false
+	// The 1s default is not always enough when running in CI.
+	runnerConf.ReadTimeout = 3 * time.Second
+
+	res, err := runner.Run(runnerConf, tests, output.NewOutput("quiet", os.Stdout))
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	if len(res.Stats.Failed) > 0 {
+		tb.Errorf("failed tests: %v", res.Stats.Failed)
+	}
+}
+
+// setupWAF starts a WAF-fronted origin server and returns the path of the error
+// log the FTW runner scans, together with the server itself.
+func setupWAF(tb testing.TB) (string, *httptest.Server) {
+	tb.Helper()
+
+	conf := coraza.NewWAFConfig()
+	customTestingConfig := `
+SecResponseBodyMimeType text/plain
+SecDefaultAction "phase:3,log,auditlog,pass"
+SecDefaultAction "phase:4,log,auditlog,pass"
+SecDefaultAction "phase:5,log,auditlog,pass"
+# Rule 900005 from https://github.com/coreruleset/coreruleset/blob/v4.0/dev/tests/regression/README.md#requirements
+SecAction "id:900005,\
+  phase:1,\
+  nolog,\
+  pass,\
+  ctl:ruleEngine=DetectionOnly,\
+  ctl:ruleRemoveById=910000,\
+  setvar:tx.blocking_paranoia_level=4,\
+  setvar:tx.crs_validate_utf8_encoding=1,\
+  setvar:tx.arg_name_length=100,\
+  setvar:tx.arg_length=400,\
+  setvar:tx.total_arg_length=64000,\
+  setvar:tx.max_num_args=255,\
+  setvar:tx.max_file_size=64100,\
+  setvar:tx.combined_file_sizes=65535"
+# Write the value from the X-CRS-Test header as a marker to the log
+# Requests with X-CRS-Test header will not be matched by any rule. See https://github.com/coreruleset/go-ftw/pull/133
+SecRule REQUEST_HEADERS:X-CRS-Test "@rx ^.*$" \
+  "id:999999,\
+  phase:1,\
+  pass,\
+  t:none,\
+  log,\
+  msg:'X-CRS-Test %{MATCHED_VAR}',\
+  ctl:ruleRemoveById=1-999999"
+`
+	// Configs are loaded with a precise order:
+	// 1. Coraza config
+	// 2. Custom Rules for testing and eventually overrides of the basic Coraza config
+	// 3. CRS basic config
+	// 4. CRS rules (on top of which are applied the previously defined SecDefaultAction)
+	conf = conf.
+		WithRootFS(coreruleset.FS).
+		WithDirectives(confRecommended).
+		WithDirectives(customTestingConfig).
+		WithDirectives("Include @crs-setup.conf.example").
+		WithDirectives("Include @owasp_crs/*.conf")
+
+	errorPath := filepath.Join(tb.TempDir(), "error.log")
+	errorFile, err := os.Create(errorPath)
+	if err != nil {
+		tb.Fatalf("failed to create error log: %v", err)
+	}
+	errorWriter := bufio.NewWriter(errorFile)
+	conf = conf.WithErrorCallback(func(rule types.MatchedRule) {
+		msg := rule.ErrorLog() + "\n"
+		if _, err := io.WriteString(errorWriter, msg); err != nil {
+			tb.Fatal(err)
+		}
+		if err := errorWriter.Flush(); err != nil {
+			tb.Fatal(err)
+		}
+	})
+
+	waf, err := coraza.NewWAF(conf)
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	// The CRS regression tests expect albedo as the backend server, the same one
+	// coraza itself runs them against. See https://github.com/coreruleset/albedo.
+	s := httptest.NewServer(txhttp.WrapHandler(waf, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		// Response body tests only work when the mime type matches SecResponseBodyMimeType above.
+		w.Header().Set("Content-Type", "text/plain")
+		albedo.Handler().ServeHTTP(w, r)
+	})))
+
+	return errorPath, s
 }
